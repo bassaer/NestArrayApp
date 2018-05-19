1f2158045b4b4dd9b074850191d238623c2e1811
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-05-19 14:43:12 +0900

    add sample

--- app/src/main/java/com/github/bassaer/nestarrayapp/MainActivity.kt
+++ app/src/main/java/com/github/bassaer/nestarrayapp/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.github.bassaer.nestarrayapp
 
 import android.os.Bundle
+import android.support.annotation.StyleableRes
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadMenu()
+    }
+
+    private fun loadMenu() {
+        val tag = "Sample"
+        val menus = resources.obtainTypedArray(R.array.menu)
+
+        @StyleableRes val nameIndex = 0
+        @StyleableRes val arrayIndex = 1
+
+        for (i in 0 until menus.length()) {
+            val menuId = menus.getResourceId(i, -1)
+            if (menuId < 0) {
+                break
+            }
+            val menu = resources.obtainTypedArray(menuId)
+
+            val arrayId = menu.getResourceId(arrayIndex, -1)
+            if (arrayId < 0) {
+                menu.recycle()
+                break
+            }
+
+            val array = resources.obtainTypedArray(arrayId)
+
+            Log.d(tag, menu.getString(nameIndex))
+            for (j in 0 until array.length()) {
+                Log.d(tag, " └── " + array.getString(j))
+            }
+            menu.recycle()
+            array.recycle()
+        }
+
+        menus.recycle()
     }
 }
